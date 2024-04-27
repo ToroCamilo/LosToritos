33fe4c5ddacf57aeb8de3dbe947acdb184f207f8
TORO <toroacevedoj@gmail.com>
2024-04-27 09:06:42 -0500

    ADD, Styles to the general page of the app from the LoginPage

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div style={{backgroundColor: "#EF1C1C", height: '100vh', overflow: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ textAlign: 'center' }}></div>
+    </div>
   )
 }
