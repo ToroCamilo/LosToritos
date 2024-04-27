b8e58b60bea87a1c76733e1bb577557ae0ed326a
TORO <toroacevedoj@gmail.com>
2024-04-27 09:10:30 -0500

    ADD, Image of the Los Toritos logo with their respective styles

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Logo from '../assets/Logo.png';
 
 export const LoginPage = () => {
   return (
     <div style={{backgroundColor: "#EF1C1C", height: '100vh', overflow: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <div style={{ textAlign: 'center' }}></div>
+            <div style={{ textAlign: 'center' }}>
+            <img src={Logo} style={{ width: '380px', height: '380px' }} />
+
+            </div>
     </div>
   )
 }
