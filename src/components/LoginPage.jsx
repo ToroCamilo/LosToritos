f32e6ab048035396bc4cc8ea55bb5819eb7fa872
TORO <toroacevedoj@gmail.com>
2024-04-27 09:15:48 -0500

    ADD, Form to Login  with username and password and a Button

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,12 +1,31 @@
+import { Form, Button } from 'react-bootstrap';
 import Logo from '../assets/Logo.png';
 
 export const LoginPage = () => {
-  return (
-    <div style={{backgroundColor: "#EF1C1C", height: '100vh', overflow: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    return (
+        <div style={{backgroundColor: "#EF1C1C", height: '100vh', overflow: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <div style={{ textAlign: 'center' }}>
-            <img src={Logo} style={{ width: '380px', height: '380px' }} />
-
+                <img src={Logo} style={{ width: '380px', height: '380px' }} />
+                <Form style={{ width: '300px', margin: '0 auto' }}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control style={{
+                            fontSize: '20px', borderRadius: '50px', backgroundColor: '#ffffff',
+                            textAlign: 'center', padding: '10px'
+                        }} type="email" placeholder="USER NAME" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control style={{
+                            fontSize: '20px', borderRadius: '50px', backgroundColor: '#ffffff',
+                            textAlign: 'center', padding: '10px'
+                        }} type="password" placeholder="PASSWORD" />
+                    </Form.Group>
+                    <div style={{ marginBottom: "40px" }}>
+                        <Button  variant="warning" type="submit" style={{ width: '35%', borderRadius: '50px' }}>
+                            LOGIN
+                        </Button>
+                    </div>
+                </Form>
             </div>
-    </div>
-  )
-}
+        </div>
+    );
+};
