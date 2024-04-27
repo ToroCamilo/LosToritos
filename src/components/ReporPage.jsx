a64edb3728a706e24bc0ed11e43e00e60001b4d8
TORO <toroacevedoj@gmail.com>
2024-04-27 09:02:39 -0500

    ADD, Folder components with files HomePage, LoginPage, InventoryPage, SalePage, ReportPage

---
+++ src/components/ReporPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ReporPage = () => {
+  return (
+    <div>ReporPage</div>
+  )
+}
---
+++ src/components/SalePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SalePage = () => {
+  return (
+    <div>SalePage</div>
+  )
+}
